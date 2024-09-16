db589ee51895c4820d730fdedeafbcee76152893
Aditya Yadav <aditya30042002yadav@gmail.com>
2024-09-16 14:42:43 +0530

    Prediction pipeline

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
-from flask import Flask,request,render_template,jsonify
-# from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
+from flask import Flask, request, render_template, jsonify
+from src.pipelines.prediction_pipeline import CustomData, PredictPipeline
 
-application=Flask(__name__)
+application = Flask(__name__)
 
-app=application
+app = application
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-@app.route('/predict',methods=['GET','POST'])
-
+@app.route('/predict', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('form.html')
     
     else:
-        data=CustomData(
-            carat=float(request.form.get('carat')),
-            depth = float(request.form.get('depth')),
-            table = float(request.form.get('table')),
-            x = float(request.form.get('x')),
-            y = float(request.form.get('y')),
-            z = float(request.form.get('z')),
-            cut = request.form.get('cut'),
-            color= request.form.get('color'),
-            clarity = request.form.get('clarity')
+        data = CustomData(
+            age=int(request.form.get('age')),
+            sex=request.form.get('sex'),
+            lithium=request.form.get('lithium'),
+            goitre=request.form.get('goitre'),
+            psych=request.form.get('psych'),
+            T3=float(request.form.get('T3')),
+            T4U=float(request.form.get('T4U')),
+            FTI=float(request.form.get('FTI')),
+            tumor=request.form.get('tumor'),
+            on_thyroxine=request.form.get('on_thyroxine'),
+            hypopituitary=request.form.get('hypopituitary'),
+            on_antithyroid_medication=request.form.get('on_antithyroid_medication'),
+            thyroid_surgery=request.form.get('thyroid_surgery'),
+            I131_treatment=request.form.get('I131_treatment')
         )
-        final_new_data=data.get_data_as_dataframe()
-        predict_pipeline=PredictPipeline()
-        pred=predict_pipeline.predict(final_new_data)
+        final_new_data = data.get_data_as_dataframe()
 
-        results=round(pred[0],2)
+        predict_pipeline = PredictPipeline()
+        pred = predict_pipeline.predict(final_new_data)
+        
+        results = round(pred[0], 2)
+        
+        return render_template('form.html', final_result=results)
 
-        return render_template('form.html',final_result=results)
-    
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ src/pipelines/prediction_pipelines.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            # Define paths to preprocessor and model files
+            preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+            model_path = os.path.join('artifacts', 'model.pkl')
+
+            # Load preprocessor and model
+            preprocessor = load_object(preprocessor_path)
+            model = load_object(model_path)
+
+            # Preprocess and predict
+            data_scaled = preprocessor.transform(features)
+            pred = model.predict(data_scaled)
+            return pred
+        except Exception as e:
+            logging.error("Exception occurred in prediction pipeline")
+            raise CustomException(e, sys)
+
+class CustomData:
+    def __init__(self,
+                 age: int,
+                 sex: str,
+                 lithium: str,
+                 goitre: str,
+                 psych: str,
+                 T3: float,
+                 T4U: float,
+                 FTI: float,
+                 tumor: str,
+                 on_thyroxine: str,
+                 hypopituitary: str,
+                 on_antithyroid_medication: str,
+                 thyroid_surgery: str,
+                 I131_treatment: str):
+        
+        self.age = age
+        self.sex = sex
+        self.lithium = lithium
+        self.goitre = goitre
+        self.psych = psych
+        self.T3 = T3
+        self.T4U = T4U
+        self.FTI = FTI
+        self.tumor = tumor
+        self.on_thyroxine = on_thyroxine
+        self.hypopituitary = hypopituitary
+        self.on_antithyroid_medication = on_antithyroid_medication
+        self.thyroid_surgery = thyroid_surgery
+        self.I131_treatment = I131_treatment
+
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'age': [self.age],
+                'sex': [self.sex],
+                'lithium': [self.lithium],
+                'goitre': [self.goitre],
+                'psych': [self.psych],
+                'T3': [self.T3],
+                'T4U': [self.T4U],
+                'FTI': [self.FTI],
+                'tumor': [self.tumor],
+                'on_thyroxine': [self.on_thyroxine],
+                'hypopituitary': [self.hypopituitary],
+                'on_antithyroid_medication': [self.on_antithyroid_medication],
+                'thyroid_surgery': [self.thyroid_surgery],
+                'I131_treatment': [self.I131_treatment]
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe gathered successfully')
+            return df
+        except Exception as e:
+            logging.error('Exception occurred prediction pipeline while gathering dataframe')
+            raise CustomException(e, sys)
